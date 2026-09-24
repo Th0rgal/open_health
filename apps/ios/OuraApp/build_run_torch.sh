1edcd6137aeec101f9158888d4b3975f92f2c2af
Thomas Marchand <thomas.marchand@tuta.io>
2026-09-24 11:05:40 +0200

    build(ios): md.thomas bundle id and link HeartRate/RawData in the torch build

--- apps/ios/OuraApp/build_run_torch.sh
+++ apps/ios/OuraApp/build_run_torch.sh
@@ -43,7 +43,8 @@ xcrun -sdk iphonesimulator swiftc -O \
     "$APPDIR/BLETransport.swift" "$APPDIR/RingSync.swift" "$APPDIR/ProfileSettings.swift" \
     "$APPDIR/EventStore.swift" "$APPDIR/ModelCache.swift" "$APPDIR/ModelProgress.swift" \
     "$APPDIR/SleepStaging.swift" "$APPDIR/CvaModel.swift" "$APPDIR/ActivityModel.swift" "$APPDIR/IllnessModel.swift" \
-    "$APPDIR/Diagnostics.swift" "$APPDIR/HealthExport.swift" "$APPDIR/DayExport.swift" "$APPDIR/WorkCoordinator.swift" "$BUILD/TorchBridge.o" "$BUILD/CrashCatch.o" \
+    "$APPDIR/Diagnostics.swift" "$APPDIR/HealthExport.swift" "$APPDIR/DayExport.swift" "$APPDIR/WorkCoordinator.swift" \
+    "$APPDIR/HeartRate.swift" "$APPDIR/RawData.swift" "$BUILD/TorchBridge.o" "$BUILD/CrashCatch.o" \
     -L "$XCF" -loura_core \
     -lc++ -lsqlite3 \
     -L "$LT/lib" -ltorch -ltorch_cpu -lc10 \
